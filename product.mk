eb83edc9f0af2b8d3667fedcb5606cb5641465b1
AdarshGrewal <adarsh@aosip.dev>
2021-06-10 11:05:33 +0530

    GMS: make gms conditional

--- product.mk
+++ product.mk
@@ -1,6 +1,8 @@
 # Google Apps
+ifneq ($(BUILD_VANILLA), true)
 WITH_GMS := true
 $(call inherit-product, vendor/gms/products/gms.mk)
+endif
 
 PRODUCT_PACKAGES += \
 	AccentColorBetterYellowOverlay \
