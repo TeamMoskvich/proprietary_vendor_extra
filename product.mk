dccb01c4ca49bc61ae3e7830c3662009814e5f07
AdarshGrewal <adarsh@aosip.dev>
2021-06-10 11:00:06 +0530

    extra: lets add some cute Accnets

--- product.mk
+++ product.mk
@@ -1,3 +1,11 @@
 # Google Apps
 WITH_GMS := true
 $(call inherit-product, vendor/gms/products/gms.mk)
+
+PRODUCT_PACKAGES += \
+	AccentColorBetterYellowOverlay \
+	AccentColorBetterRedOverlay \
+	AccentColorBetterPinkOverlay \
+	AccentColorBetterOrangeOverlay \
+	AccentColorBetterGreenOverlay \
+	AccentColorBetterBlueOverlay
