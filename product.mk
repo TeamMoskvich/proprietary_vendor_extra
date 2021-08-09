0df5780eca427c1dead6ad952ab1ff6110febf46
AdarshGrewal <adarshgrewal@gmail.com>
2021-08-09 20:06:34 +0530

    extra: overlay refresh rate strings
    * to display 120 Hz instead of 90 Hz

--- product.mk
+++ product.mk
@@ -4,6 +4,11 @@ WITH_GMS := true
 $(call inherit-product, vendor/gms/products/gms.mk)
 endif
 
+# Overlay
+PRODUCT_PACKAGES += \
+	SettingsOverlayRR
+
+# Accents
 PRODUCT_PACKAGES += \
 	AccentColorBetterYellowOverlay \
 	AccentColorBetterRedOverlay \
