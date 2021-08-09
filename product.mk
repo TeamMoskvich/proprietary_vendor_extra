72ee69899b180f055b6668a611c9aeefcd6b9291
AdarshGrewal <adarshgrewal@gmail.com>
2021-08-09 20:06:34 +0530

    extra: overlay ANXCamera strings
    * to display Camera instead of ANXCamera

--- product.mk
+++ product.mk
@@ -14,6 +14,10 @@ WITH_GMS := true
 $(call inherit-product, vendor/gms/products/gms.mk)
 endif
 
+# Overlay
+PRODUCT_PACKAGES += \
+	ANXCameraOverlay
+
 # PixelPropsUtils
 PRODUCT_COPY_FILES += \
     vendor/extra/prebuilt/common/etc/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
