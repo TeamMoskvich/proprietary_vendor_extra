298a00e710acd15de5ec58372908418aee685d11
Maxim Ivanov <m.ivanov2k@gmail.com>
2021-12-05 21:19:06 +0200

    extra: remove gms

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,7 +1,2 @@
 # ANXCamera
 include vendor/ANXCamera/BoardConfigAnx.mk
-
-# Google Apps
-ifneq ($(BUILD_VANILLA), true)
-include vendor/gms/products/board.mk
-endif
--- product.mk
+++ product.mk
@@ -8,12 +8,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64
 
-# Google Apps
-ifneq ($(BUILD_VANILLA), true)
-WITH_GMS := true
-$(call inherit-product, vendor/gms/products/gms.mk)
-endif
-
 # PixelPropsUtils
 PRODUCT_COPY_FILES += \
     vendor/extra/prebuilt/common/etc/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
